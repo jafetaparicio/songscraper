639739a8e26c65942bce9d460fd511d8b59f8f0d
Jafet Aparicio <jafet.aparicio1@gmail.com>
2020-05-07 09:35:29 -0400

    code that was working and not anymore

--- Lyrics.py
+++ Lyrics.py
@@ -7,6 +7,7 @@ import os
 #Import the package and search for songs by a given artist:
 import lyricsgenius
 import re
+import time
 
 path = "/Users/jafetaparicio/OneDrive - Eastern Connecticut State University/College/Spring2020/Thesis/Lyrics/"
 os.chdir(path)
@@ -21,36 +22,62 @@ os.chdir(path)
 
 
 songListDir = '/Users/jafetaparicio/OneDrive - Eastern Connecticut State University/College/Spring2020/Thesis/Lyrics/songscraper/SongLists'
-os.path.join(songListDir, '2019.txt')
-#reading url file and creating list
+songSearchDir = '/Users/jafetaparicio/OneDrive - Eastern Connecticut State University/College/Spring2020/Thesis/Lyrics/songscraper/searched'
+searchDir = '/Users/jafetaparicio/OneDrive - Eastern Connecticut State University/College/Spring2020/Thesis/Lyrics/songscraper/searched'
+
+
+
 file = '2019.txt'
+
 with open(os.path.join(songListDir, '2019.txt')) as infile :
         songList = infile.readlines()
         infile.close()
         
-        
+songs = []
 
-for line in songList:
-    songs = line.split("\t")
+for line in songList :
+    line = line.strip("\"")
+    line = line.strip("\n")
+    line = line.strip("\"")
+    songs.append(line.split("\t"))
     
+#Songs is new song list
+#Song title: songs[#][0]
+#Artist: songs[#][1]
 
-
-    
-
-    
-songs[0] = songs[0].strip("\"")
-songs[1] = songs[1].strip("\n")
 
 '''
 #file of song name and artist searched and year(hot100), and song name and artist actually searched and year
 '''
+
+genius = lyricsgenius.Genius("eNHgmMCeRrd8prhC7EnyZtxX6Y_Ek8HOjMq8AS_6WCX4aiRhiqQiXYDukJcgyZcb")
+artist = genius.search_artist(songs[98][1], max_songs=1, sort="title")
+song = genius.search_song(songs[98][0], songs[98][1])
+
+
+
+
+with open(os.path.join(songSearchDir, 'search.txt'), 'a')  as f:
+    f.write(songs[98][0] + ' '+ songs[98][1] + ' ****** ' +song.title + ' '  + song.artist + "\n")
+    f.close()
+
+
+
+
 #add year param
 def getLyrics(songName, artist, year):
     genius = lyricsgenius.Genius("eNHgmMCeRrd8prhC7EnyZtxX6Y_Ek8HOjMq8AS_6WCX4aiRhiqQiXYDukJcgyZcb")
     artist = genius.search_artist(artist, max_songs=1, sort="title")
-    song = genius.search_song(songName, artist.name)
+    song = genius.search_song(songName, artist)
+    
+    with open(os.path.join(songSearchDir, 'search.txt'), 'a')  as f:
+        f.write(songName + ' '+ artist + ' ****** ' + str(song.title) + ' '  + str(song.artist) + "\n")
+        f.close()
+    
     if song == None:
-        print("error")
+        with open(os.path.join(songSearchDir, 'error.txt'), 'a')  as f:
+            f.write(songName + ' ' +song.title + "\n")
+            f.close()
         #save to error file
     else:
         f= open(songName + '.txt' ,"w+")
@@ -60,9 +87,24 @@ def getLyrics(songName, artist, year):
     #wait half second before next request
     time.sleep(.5)
     return song
-    
-    
-s = getLyrics("Te quiero", "Lizzo")
+
+      
 
 
 
+def songLyrics(songFile, songListDir, searchDir):
+    with open(os.path.join(songListDir, songFile)) as infile :
+        songList = infile.readlines()
+        infile.close()
+        
+        songs = []
+    for line in songList :
+        line = line.strip("\"")
+        line = line.strip("\n")
+        line = line.strip("\"")
+        songs.append(line.split("\t"))
+        
+    for i in range(0,100):
+        getLyrics(songs[i][0], songs[i][1], searchDir)
+
+songLyrics("2019.txt", songListDir, searchDir)
